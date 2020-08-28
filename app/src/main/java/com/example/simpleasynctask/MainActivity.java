c7cba31d8183ae1f26a61afdc5c8fe3afca42af1
Raymond Baranda <rbaranda22@hotmail.com>
2020-08-28 18:09:47 -0500

    Initial commit

---
+++ app/src/main/java/com/example/simpleasynctask/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.simpleasynctask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    mTextView = findViewById(R.id.textView1);
+
+    public void startTask(View view) {
+        // Put a message in the text view
+        mTextView.setText(R.string.napping);
+
+        new SimpleAsyncTask(mTextView).execute();
+
+        @Override
+        protected void onSaveInstanceState(Bundle outState) {
+            super.onSaveInstanceState(outState);
+            // Save the state of the TextView
+            outState.putString(TEXT_STATE, mTextView.getText().toString());
